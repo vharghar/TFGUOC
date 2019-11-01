1213faaf0faf8bf710d86a4d4b263aa78ac4df7a
tonivr <tonivr0@gmail.com>
2019-11-01 20:28:53 +0100

    1-11

---
+++ LEEME-README.md
@@ -0,0 +1,9 @@
+Demo 4x4land
+
+para ejecutar la demo hay dos opciones
+
+1 descargar la carpeta juego
+2 descargar 4x4land.zip y descomprimir 
+
+en ambos casos ejecutar el archivo tfguoc.exe para arramcar el juego, 
+no es necesario ningun tipo de instalacion ni archivos adicionales
---
+++ TFGUOC/Assets/Scripts/mostraFPS.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mostraFPS : MonoBehaviour
+{
+    public Text fpsText;
+    public float deltaTime;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text ="FPS = " + Mathf.Ceil(fps).ToString();
+    }
+}
